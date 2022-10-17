8514d3bef7132351c83c430b49e5ec8d7ae0486d
Milad Rezaeian <milad.rezaeiann@gmail.com>
2022-10-17 12:24:56 +0330

    show old form input after submit

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -12,27 +12,27 @@
                 <div class="row">
                     <div class="col-md-6">
                         <label>@lang('videos.name')</label>
-                        <input type="text" name="name" class="form-control" placeholder="@lang('videos.name')">
+                        <input type="text" name="name" class="form-control" value="{{old("name")}}" placeholder="@lang('videos.name')">
                     </div>
                     <div class="col-md-6">
                         <label>@lang('videos.length')</label>
-                        <input type="text" name="length" class="form-control" placeholder="@lang('videos.length')">
+                        <input type="text" name="length" class="form-control" value="{{old("length")}}" placeholder="@lang('videos.length')">
                     </div>
                     <div class="col-md-6">
                         <label>نام یکتا</label>
-                        <input type="text" name="slug" class="form-control" placeholder="نام یکتا">
+                        <input type="text" name="slug" class="form-control" value="{{old("slug")}}" placeholder="نام یکتا">
                     </div>
                     <div class="col-md-6">
                         <label>آدرس ویدیو</label>
-                        <input type="text" name="url" class="form-control" placeholder="آدرس ویدیو">
+                        <input type="text" name="url" class="form-control" value="{{old("url")}}" placeholder="آدرس ویدیو">
                     </div>
                     <div class="col-md-6">
                         <label>تصویر بندانگشتی</label>
-                        <input type="text" name="thumbnail" class="form-control" placeholder="تصویر بندانگشتی">
+                        <input type="text" name="thumbnail" class="form-control" value="{{old("thumbnail")}}" placeholder="تصویر بندانگشتی">
                     </div>
                     <div class="col-md-12">
                         <label>توضیحات</label>
-                        <textarea name="description" class="form-control" rows="4" placeholder="توضیح"></textarea>
+                        <textarea name="description" class="form-control" rows="4" placeholder="توضیح">{{old("name")}}</textarea>
                     </div>
                     <div class="col-md-6">
                         <button type="submit" id="contact_submit" class="btn btn-dm">ذخیره</button>
