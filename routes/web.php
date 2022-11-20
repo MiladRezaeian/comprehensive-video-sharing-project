f1e68ee054f6ae57568b3735174f727f8c579ca0
Milad Rezaeian <milad.rezaeiann@gmail.com>
2022-11-20 16:51:29 +0330

    add laravel breeze files after install and fix bugs

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+<<<<<<< HEAD
+=======
+use App\Http\Controllers\CategoryVideoController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\VideoController;
+use App\Models\Video;
+>>>>>>> d016f20 (add laravel breeze files after install and fix bugs)
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +35,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+<<<<<<< HEAD
+=======
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+>>>>>>> d016f20 (add laravel breeze files after install and fix bugs)
 require __DIR__.'/auth.php';
