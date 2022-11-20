2adfc0c67979053370d265244e4008348e1be581
Milad Rezaeian <milad.rezaeiann@gmail.com>
2022-11-20 15:39:17 +0330

    add laravel breeze files after install

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryVideoController;
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\VideoController;
-use App\Models\Video;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,24 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', [IndexController::class, 'index']);
-
-Route::get('/', [IndexController::class, 'index'])->name('index');
-
-Route::get('/videos/create', [VideoController::class, 'create'])->name('videos.create');
-Route::post('/videos', [VideoController::class, 'store'])->name('videos.store');
-Route::get('/videos/{video}', [VideoController::class, 'show'])->name('videos.show');
-Route::get('/videos/{video}/edit', [VideoController::class, 'edit'])->name('videos.edit');
-Route::post('/videos/{video}/', [VideoController::class, 'update'])->name('videos.update');
-
-Route::get('/categories/{category:slug}/videos', [CategoryVideoController::class, 'index'])->name('categories.videos.index');
-
-Route::get('/upload', function (){
-    return view('videos.create');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/videos', [VideoController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/factory', function (){
-    Video::factory()->create();
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
