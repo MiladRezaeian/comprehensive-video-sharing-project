115d5a5b57964477248b967d71147188120c3c37
Milad Rezaeian <milad.rezaeiann@gmail.com>
2022-11-27 18:14:29 +0330

    Customize verify email page

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -23,5 +23,6 @@ class EmailVerificationNotificationController extends Controller
         $request->user()->sendEmailVerificationNotification();
 
         return back()->with('status', 'verification-link-sent');
+        return back()->with('status', __('messages.verification_email_was_sent'));
     }
 }
