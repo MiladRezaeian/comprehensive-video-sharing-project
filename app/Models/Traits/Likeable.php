8bd2b20d8bc7bf7bb449ec208f740077542b897b
Mateo Miller <mateo.m@realtyna.com>
2024-05-02 09:22:36 -0400

    show likes of video

--- app/Models/Traits/Likeable.php
+++ app/Models/Traits/Likeable.php
@@ -12,4 +12,18 @@ trait Likeable
     {
         return $this->morphMany(Like::class, 'likeable');
     }
+
+    public function getLikesCountAttribute()
+    {
+        return $this->likes()
+            ->where('vote', 1)
+            ->count();
+    }
+
+    public function getDisLikesCountAttribute()
+    {
+        return $this->likes()
+            ->where('vote', -1)
+            ->count();
+    }
 }
