3e36cf2e4baa2ed4d4fdf218ed41353f092f5833
Mateo Miller <mateo.m@realtyna.com>
2024-05-04 10:14:52 -0400

    Customize 'likeable_id' route model binding

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -6,14 +6,9 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    public function store(Request $request, string $likeable_type, string $likeable_id)
+    public function store(Request $request, string $likeable_type, $likeable_id)
     {
-        $model_name = 'App\\Models\\' . ucfirst($likeable_type);
-        $routeKey = (new $model_name)->getRouteKeyName();
-
-        $likeable = $model_name::where($routeKey, $likeable_id)->firstOrfail();
-
-        $likeable->Likes()->create([
+        $likeable_id->Likes()->create([
             'user_id' => auth()->id(),
             'vote' => 1
         ]);
