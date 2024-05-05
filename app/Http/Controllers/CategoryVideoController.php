71fb831dcb3c28640a28316ac3dfe1e634c7e2bf
Mateo Miller <mateo.m@realtyna.com>
2024-05-05 04:05:19 -0400

    add filter to categories page

--- app/Http/Controllers/CategoryVideoController.php
+++ app/Http/Controllers/CategoryVideoController.php
@@ -7,9 +7,21 @@ use Illuminate\Http\Request;
 
 class CategoryVideoController extends Controller
 {
-    public function index(Category $category)
+    public function index(Request $request, Category $category)
     {
-        $videos = $category->videos()->paginate();
+        $videos = $category->videos();
+
+        if ($request->has('length') && $request->length == 1) {
+            $videos = $videos->where('length', '<', 60);
+        }
+        if ($request->has('length') && $request->length == 2) {
+            $videos = $videos->whereBetween('length', [60, 300]);
+        }
+        if ($request->has('length') && $request->length == 2) {
+            $videos = $videos->where('length', '>', 300);
+        }
+
+        $videos = $videos->paginate();
         $title = $category->name;
 
         return view('videos.index', compact('videos', 'title'));
--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 
+    <form action="#" class="mt-5 main-page-filter" method="get" style="padding: 35px 0px 0px;">
+        <div class="row">
+            <div class="form-group col-md-3">
+                <label for="inputCity">ترتیب بر اساس</label>
+                <select class="form-control" name="sortBy">
+                    <option {{ request()->query('sortBy') == 'created_at' ? 'selected' : '' }} value="created_at">جدیدترین</option>
+                    <option {{ request()->query('sortBy') == 'like' ? 'selected' : '' }} value="like">محبوب ترین</option>
+                    <option {{ request()->query('sortBy') == 'length' ? 'selected' : '' }} value="length">مدت زمان ویدیو</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="inputState">مدت زمان</label>
+                <select class="form-control" name="length">
+                    <option {{ request()->query('length') == null ? 'selected' : '' }} value="">همه</option>
+                    <option {{ request()->query('length') == 1 ? 'selected' : '' }} value="1">کمتر از یک دقیقه</option>
+                    <option {{ request()->query('length') == 2 ? 'selected' : '' }} value="2">یک تا پنج دقیقه</option>
+                    <option {{ request()->query('length') == 3 ? 'selected' : '' }} value="3">بیشتر از پنج دقیقه</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3" style="margin-top: 29px">
+                <button type="submit" class="btn btn-primary">فیلتر</button>
+            </div>
+        </div>
+    </form>
+
     <h1 class="new-video-title"><i class="fa fa-bolt"></i> {{ $title }} </h1>
     <div class="row">
         @foreach($videos as $video)
